8a7aefcfe6f7f33013a3d2d21a74ea5f8b2c3a67
CSE LAB <CSE LAB@ACER>
2025-02-28 15:26:29 +0530

    commit

---
+++ Usingtelegramapi/src/main/java/messageusingtelegram/Usingtelegramapi/UsingtelegramapiApplication.java
@@ -0,0 +1,13 @@
+package messageusingtelegram.Usingtelegramapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsingtelegramapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsingtelegramapiApplication.class, args);
+	}
+
+}
---
+++ Usingtelegramapi/src/test/java/messageusingtelegram/Usingtelegramapi/UsingtelegramapiApplicationTests.java
@@ -0,0 +1,13 @@
+package messageusingtelegram.Usingtelegramapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsingtelegramapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
